0511af28ea6dc907392bbd86bd695d55719b57d7
AlgoVoyager <nishantdewangan2002@gmail.com>
2022-02-27 22:37:00 +0530

    new features added

--- AlgoSpammer.py
+++ AlgoSpammer.py
@@ -11,36 +11,73 @@ def start_art(additional):
     print(art+"\n"+ additional)
 
 try:
-    input_word = sys.argv[1]
+    input_word = sys.argv[1] 
     if input_word =="-uf" or input_word=="-usefile":
         try:
+            
             filename = sys.argv[2]
             
-            printtext="using the custom text file "+filename+" to spam in 5 secs...\n press CTRL+C to terminate.\n"
-            start_art(printtext)
-            sleep(5)
-            if os.path.isfile(filename):
-                custom_file = open(filename,"r")
-                for line in custom_file:
+            try:
+                spam_count =sys.argv[3]
+                printtext="using the custom text file "+str(filename)+" "+str(spam_count)+" times to spam in 5 secs...\n press CTRL+C to terminate.\n"
+                start_art(printtext)
+                
+                if os.path.isfile(filename):
+                    sleep(5)
+                    
+                    for cnt in range(int(spam_count)):
+                        custom_file = open(filename,"r")
+                        for line in custom_file:
+                            pg.typewrite(line)
+                            pg.press("enter")
+                        custom_file.close()
+                    print("spamming through file done...")
+                else:
+                    sleep(0.5)
+                    print("file doesn't exit, exiting spammer...")
+                    exit()
+
+            except IndexError:
+                printtext="using the custom text file "+filename+" only once to spam in 5 secs...\n press CTRL+C to terminate.\n"
+                start_art(printtext)
+                
+                if os.path.isfile(filename):
+                    sleep(5)
+                    custom_file = open(filename,"r")
+                    for line in custom_file:
+                        pg.typewrite(line)
+                        pg.press("enter")
+                    custom_file.close()
+                    print("spamming through file done...")
+                else:
+                    sleep(0.5)
+                    print("file doesn't exit, exiting spammer...")
+                    exit()
+        except IndexError:
+            filename2= "spamtext.txt"
+            try:
+                spam_count =sys.argv[2]
+                
+                printtext="using default spamtext file "+str(spam_count)+" times in 5 secs...\n press CTRL+C to terminate.\n"
+                start_art(printtext)
+                sleep(5)
+                for cnt in range(int(spam_count)):
+                    f= open(filename2,"r")
+                    for line in f:
+                        pg.typewrite(line)
+                        pg.press("enter")
+                    f.close()
+                print("spamming through file done, exiting spammer...")
+            except IndexError:
+                printtext="using default spamtext file only once in 5 secs...\n press CTRL+C to terminate.\n"
+                start_art(printtext)
+                sleep(5)
+                f= open(filename2,"r")
+                for line in f:
                     pg.typewrite(line)
                     pg.press("enter")
-                custom_file.close()
-                print("spamming through file done...")
-            else:
-                print("file doesn't exit, exiting spammer...")
-                exit()
-        except IndexError:
-            filename= "spamtext.txt"
-            
-            printtext="using default spamtext file in 5 secs...\n press CTRL+C to terminate.\n"
-            start_art(printtext)
-            sleep(5)
-            f= open(filename,"r")
-            for line in f:
-                pg.typewrite(line)
-                pg.press("enter")
-            f.close()
-            print("spamming through file done, exiting spammer...")
+                f.close()
+                print("spamming through file done, exiting spammer...")
     else:
         try:
             count = sys.argv[2]
